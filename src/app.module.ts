edb16dd04948dce2a5a533358f93401508d51ec0
Gabriel <gabriel.perino99@gmail.com>
2023-12-06 16:31:33 -0300

    chore: config graphql server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,40 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
+import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'schema.gql'),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        entities: [__dirname + '/../../**/*.entity.{ts,js}'],
+        migrations: [__dirname + '/../migrations/**/*{.ts,.js}'],
+        migrationsRun: true,
+        synchronize: true,
+        logging: true,
+      }),
+    }),
+    AppResolver,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
